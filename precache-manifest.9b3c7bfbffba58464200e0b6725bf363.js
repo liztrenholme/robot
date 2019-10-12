895f30c7c96bbe0e0611fda0eb72ec034c3721bd
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-10-12 14:35:31 -0400

    Updates

---
+++ precache-manifest.9b3c7bfbffba58464200e0b6725bf363.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b954bc17a2f3f713deeb02364718326",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "d9fafdc934d461eb190b",
+    "url": "/robot/static/css/main.a5563f1e.chunk.css"
+  },
+  {
+    "revision": "7e76451350b93e002c8f",
+    "url": "/robot/static/js/2.31e57c17.chunk.js"
+  },
+  {
+    "revision": "d9fafdc934d461eb190b",
+    "url": "/robot/static/js/main.368939ea.chunk.js"
+  },
+  {
+    "revision": "585b578b20bceaa7888a",
+    "url": "/robot/static/js/runtime-main.e653ca2d.js"
+  },
+  {
+    "revision": "0c2bfaa258656978dbdd620df331be76",
+    "url": "/robot/static/media/arm.0c2bfaa2.png"
+  },
+  {
+    "revision": "aa4751ba28a71b9e203a3f904f458b92",
+    "url": "/robot/static/media/body.aa4751ba.png"
+  },
+  {
+    "revision": "937d0fcd410a14fd93af24083824864c",
+    "url": "/robot/static/media/head.937d0fcd.png"
+  },
+  {
+    "revision": "3a8a3d1be61a95a41089621c498c5e8f",
+    "url": "/robot/static/media/leg.3a8a3d1b.png"
+  }
+]);
