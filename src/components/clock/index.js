08fc36512de005be09f0428e25e8d50dcd9dfa56
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-10-18 20:59:04 -0400

    added button to toggle between standard and military time

--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -4,7 +4,8 @@ import './clock.css'
 
 class Clock extends Component {
     state = {
-      date: ''
+      date: '00:00:00',
+      standard: true
     }
     componentDidMount() {
       this.clockInterval = setInterval(this.updateTime, 1000)
@@ -13,21 +14,29 @@ class Clock extends Component {
     componentWillUnmount() {
       clearInterval(this.clockInterval)
     }
+
+    switchStandard = () => this.state.standard ? this.setState({standard: false}) : this.setState({standard: true})
     
     updateTime = () => {
       const base = new Date()
       const hour = base.getHours().toString().length === 1 ? `0${base.getHours()}` : base.getHours()
+      const standardHour = hour > 12 ? hour - 12 : hour === '00' ? '12' : hour
       const minute = base.getMinutes().toString().length === 1 ? `0${base.getMinutes()}` : base.getMinutes()
-      // this.setState({date: hour})
       const seconds = base.getSeconds().toString().length === 1 ? `0${base.getSeconds()}` : base.getSeconds()
-      const date = `${hour}:${minute}:${seconds}`
+      const suffix = hour > 11 ? 'PM' : 'AM'
+      const date = this.state.standard ? `${standardHour}:${minute}:${seconds} ${suffix}` : `${hour}:${minute}:${seconds}`
       this.setState({date})
     }
     render() {
-      console.log(this.state.date)
+      const {standard, date} = this.state
       return (
-        <div className='clock-container'>
-          <h2>{this.state.date}</h2>
+        <div className='outer-container'>
+          <div onClick={this.switchStandard} className='button'>
+            {standard ? 'Switch to Military' : 'Switch to Standard'}
+          </div>
+          <div className='clock-container'>
+            <h2>{date}</h2>
+          </div>
         </div>
       )
     }
